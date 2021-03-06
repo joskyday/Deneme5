fa85ae124b941430c9cb3952e44db8c14ed1cf90
joskyday <71334485+joskyday@users.noreply.github.com>
2021-03-06 13:49:52 +0300

    Update LoginStepDefs.java

--- src/test/java/com/fleetgru/stepdefinitions/LoginStepDefs.java
+++ src/test/java/com/fleetgru/stepdefinitions/LoginStepDefs.java
@@ -11,7 +11,7 @@ import org.junit.Assert;
 
 public class LoginStepDefs {
     @Given("the user is on the login page")
-    public void the_user_is_on_the_login_page() throws InterruptedException {
+    public void the_user_is_on_the_login_page()  {
         String url = ConfigurationReader.get("url");
         //WebDriver driver = Driver.get();
         Driver.get().get(url);
@@ -19,7 +19,7 @@ public class LoginStepDefs {
     }
 
     @When("the user enters the driver information")
-    public void the_user_enters_the_driver_information() throws InterruptedException {
+    public void the_user_enters_the_driver_information()  {
         String username = ConfigurationReader.get("driver_username");
         String password = ConfigurationReader.get("driver_password");
 
@@ -28,7 +28,7 @@ public class LoginStepDefs {
     }
 
     @Then("the user should be able to login")
-    public void the_user_should_be_able_to_login() throws InterruptedException {
+    public void the_user_should_be_able_to_login()  {
         BrowserUtils.waitFor(3);
         String actualTitle = Driver.get().getTitle();
         Assert.assertEquals("Dashboard",actualTitle);
@@ -36,7 +36,7 @@ public class LoginStepDefs {
     }
 
     @When("the user enters the sales manager information")
-    public void the_user_enters_the_sales_manager_information() throws InterruptedException {
+    public void the_user_enters_the_sales_manager_information()  {
         String username = ConfigurationReader.get("sales_manager_username");
         String password = ConfigurationReader.get("sales_manager_password");
 
